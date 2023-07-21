67fe098abe0e6372606ad61c40b2055443b2ccd7
Yansa27 <juliansa837@gmail.com>
2023-07-21 21:17:12 +0700

    rest full express

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ app.get('/' , (req , res) => {
     res.render('home')
 })
 
+app.get('/order' , (req , res) => {
+    res.send('Methode get is running')
+})
+app.post('/order' , (req , res) => {
+    res.send('Methode post is running')
+})
+
 app.get('/rand' , (req , res) => {
     const num = Math.floor(Math.random() * 10) + 1 ;
     res.render('random' , {random: num})
